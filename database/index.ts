b8cb6734285f88dd2227a6dd9f00db652b58f38f
dipak <dipakmahajan2808200>
2024-10-25 23:08:07 +0530

    first commit

--- database/index.ts
+++ database/index.ts
@@ -1,7 +1,26 @@
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
+// Setup mongodb
+const configs: any = {
+  development: {
+    connection: process.env.DATABASE_CONNECTION_URI_DEV,
+  },
 
-export const mongooseConnection  = async () =>{
-    return  await mongoose.connect("mongodb://localhost:27017/demo")
-}
+  test: {
+    connection: process.env.DATABASE_CONNECTION_URI_TEST,
+  },
+
+  staging: {
+    connection: process.env.DATABASE_CONNECTION_URI_STAGE,
+  },
+};
+
+const config = configs[process.env.NODE_ENV || 'development'].connection;
+/** Connect to Mongo */
+export const mongooseConnection = async () => {
+  mongoose.set('strictQuery', false);
+  return mongoose.connect(config);
+};
