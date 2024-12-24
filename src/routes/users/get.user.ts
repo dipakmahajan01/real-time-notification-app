dff2d147dc60a084e7c60cb24f16c838d1bc31d7
dipak <dipakmahajan2808200>
2024-12-25 01:46:32 +0530

    get user

--- src/routes/users/get.user.ts
+++ src/routes/users/get.user.ts
@@ -2,10 +2,17 @@ import { StatusCodes } from "http-status-codes";
 import { createJwtToken } from "../../helper/validations/jwt.helper";
 import { responseGenerators } from "../../lib";
 import { Request, Response } from "express";
+import { ERROR } from "../../common/global-constants";
 
-const createToken = (req: Request, res: Response) => {
+export const createToken = (req: Request, res: Response) => {
+  try {
     const jwtToken = createJwtToken({
       user_id: '1',
     } as any);
     return res.status(StatusCodes.OK).send(responseGenerators(jwtToken, StatusCodes.OK, 'create token', false));
-  });
+  } catch (error) {
+    return res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .send(responseGenerators({}, StatusCodes.INTERNAL_SERVER_ERROR, ERROR.INTERNAL_SERVER_ERROR, true));
+  }
+}
--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,6 +1,7 @@
 import Router from 'express';
 import { createToken } from './get.user';
 
+
 const userRoutes = Router();
 userRoutes.get('/create-token',createToken);
 
