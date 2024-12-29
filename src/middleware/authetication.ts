2c0c3a4bea24c403949500cfd1ea76750feb4f8c
dipak <dipakmahajan2808200>
2024-12-29 08:57:47 +0530

    try catch added in middelware

--- src/middleware/authetication.ts
+++ src/middleware/authetication.ts
@@ -1,20 +1,24 @@
-import { verifyJwt } from "../helper/validations/jwt.helper";
+import { StatusCodes } from 'http-status-codes';
+import { verifyJwt } from '../helper/validations/jwt.helper';
+import { Response } from 'express';
+import { responseGenerators } from '../lib';
+import { ERROR } from '../common/global-constants';
 
-export const socketAuthenticateUser = async (socket) => {
-    try {
-      const { authorization } = socket.handshake.headers;
-      if (!authorization) {
-        return false;
-      }
-      const tokenData = await verifyJwt(authorization);
-      if (!tokenData) {
-        return false;
-      }
-      socket.handshake.headers.tokenData = tokenData as any;
-      return true;
-    } catch (error: any) {
-      console.log("error",error)
-      return false
+export const socketAuthenticateUser = async (socket, res?: Response) => {
+  try {
+    const { authorization } = socket.handshake.headers;
+    if (!authorization) {
+      return false;
     }
-  };
-  
+    const tokenData = await verifyJwt(authorization);
+    if (!tokenData) {
+      return false;
+    }
+    socket.handshake.headers.tokenData = tokenData as any;
+    return true;
+  } catch (error: any) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .send(responseGenerators({}, StatusCodes.UNAUTHORIZED, 'unauthorized', true));
+  }
+};
